4b60117794f508cc6636ed31b1478d458b857465
Melby Sjamsuddin <melbysjamsuddin@Quiksilver.local>
2013-12-15 09:38:38 +1000

    fixed coding style.

--- examples/Client/CallCentres.php
+++ examples/Client/CallCentres.php
@@ -1,20 +1,18 @@
 <?php
 
 use Zend\Json\Json;
-use Gji\Http\RestClient;
+use Mslib;
 
-class CallCentres extends RestClient {
-
-	public function getCallCentres()
-	{
-		$this->setResource('callcentres');
-		$response = $this->sendGet();
-		if (!$response->isOk())
-			$centres = array('call_centres' => array(),
-				'total_count' => 0);
-		else
-			$centres = Json::decode($response->getBody(),
-				Json::TYPE_ARRAY);
-		return $centres;
-	}
+class CallCentres extends Mslib\Http\RestClient
+{
+    public function getCallCentres()
+    {
+        $this->setResource('callcentres');
+        $response = $this->sendGet();
+        if (!$response->isOk())
+            $centres = array('call_centres' => array(), 'total_count' => 0);
+        else
+            $centres = Json::decode($response->getBody(), Json::TYPE_ARRAY);
+        return $centres;
+    }
 }
